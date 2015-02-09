84eda73676a51797b2b927a84931d208c7bab87d
Kevin Lin <kevinptt0323@gmail.com>
2015-02-09 18:22:14 +0800

    set 2 spaces as indent

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ var app = express()
 var socket = '/tmp/' + process.env.USER + '.dev.nodeapp.com.sock'
 var mask = process.umask(0)
 if (fs.existsSync(socket))
-    fs.unlinkSync(socket)
+  fs.unlinkSync(socket)
 
 var index = require('./routes/index')
 var users = require('./routes/users')
 
 // view engine setup
+app.engine('.html', require('ejs').__express);
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('view engine', 'html')
 
 app.use('/', index)
 app.use('/users', users)
@@ -22,9 +23,9 @@ app.use(express.static(__dirname + '/public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -32,34 +33,34 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('pages/error', {
-            title: 'Error (((;OAO)))',
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('pages/error', {
+      title: 'Error (((;OAO)))',
+      message: err.message,
+      error: err
     });
+  });
 }
 // production error handler
 // no stacktraces leaked to user
 else {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('pages/error', {
-            title: 'Error',
-            message: '',
-            error: {}
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('pages/error', {
+      title: 'Error',
+      message: '',
+      error: {}
     });
+  });
 }
 
 var server = app.listen(socket, function () {
-    if( mask ) {
-        process.umask(mask)
-        mask = null
-    }
-    console.log('Example app listening at %s', server.address())
+  if( mask ) {
+    process.umask(mask)
+    mask = null
+  }
+  console.log('Example app listening at %s', server.address())
 
 })
 
